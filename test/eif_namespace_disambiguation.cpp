6bcca3f9c65d2d70b0cfcc23dcc5f05cda8e6eba
Daniela Engert <dani@ngrt.de>
2014-12-31 18:22:27 +0100

    get rid of most msvc level-4 warnings in test code
    Signed-off-by: Daniela Engert <dani@ngrt.de>
    (cherry picked from commit 31601d485fcc6d706c276af406d3b3d2a5d1c5f9)

--- test/eif_namespace_disambiguation.cpp
+++ test/eif_namespace_disambiguation.cpp
@@ -26,13 +26,13 @@ template<class T> struct not_
 namespace A {
   template<class T>
   typename enable_if<is_arithmetic<T>, bool>::type
-  arithmetic_object(T t) { return true; }
+  arithmetic_object(T) { return true; }
 }
 
 namespace B {
   template<class T>
   typename enable_if<not_<is_arithmetic<T> >, bool>::type
-  arithmetic_object(T t) { return false; }
+  arithmetic_object(T) { return false; }
 }
 
 int main()
